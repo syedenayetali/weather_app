928f4c14d3316e67053a5f0d401cfe91dc3817e9
syedenayetali <47559808+syedenayetali@users.noreply.github.com>
2021-09-14 05:29:51 +0530

    Delete js directory

--- js/CONF.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = "cd5180ded5d402ab8e337e131fdd4654";
-export const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
-// https://openweathermap.org/current
-//`https://api.openweathermap.org/data/2.5/weather?q=london&appid=cd5180ded5d402ab8e337e131fdd4654
